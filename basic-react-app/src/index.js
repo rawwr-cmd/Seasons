9b9b819b3a1e6264e71c78f07e5100aa990e9ff2
rawwr <akshayace0302@gmail.com>
2022-05-10 19:49:15 +0530

    specifying default props

--- basic-react-app/src/index.js
+++ basic-react-app/src/index.js
@@ -22,7 +22,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
